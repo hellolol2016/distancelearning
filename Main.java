5915554d630e85b2f50c431df93d4a93edacc83e
hellolol2016 <dennisthederp812@gmail.com>
2020-11-14 08:57:35 -0800

    a

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){ //main function where it creates a new window and stuff
+
+    }
+
+    public String create(String name, String notes[], String homework){
+        System.out.println("What do you want your new subject to be called?");
+        return sstufff;
+    }
+}
